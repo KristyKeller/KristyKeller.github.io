52b8b90fdf29706e0d4f46262289166c8f1dcc42
Kristy Keller <keller.kristy@outlook.com>
2020-11-12 21:22:27 -0500

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -13,8 +13,8 @@ class About extends Component {
                 {data.ShowAboutImage ? <img class="about-img" src={data.aboutImage} alt='about imaghe'></img> : null}
                 </div>
                 <div>
-                    <p>{data.abouttext[0]}</p><p>{data.abouttext[1]}</p>
-            <p>{data.abouttext[2]}</p><p>{data.abouttext[3]}</p><br></br>
+                    <p>{data.abouttext[0]}</p><br></br><p>{data.abouttext[1]}</p><br></br>
+            <p>{data.abouttext[2]}</p><br></br><p>{data.abouttext[3]}</p><br></br>
             </div>
             </Fade>
             </div>
