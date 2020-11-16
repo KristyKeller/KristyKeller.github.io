b53010ab2940ceea9deff35e05800856b0eccd3f
Kristy Keller <keller.kristy@outlook.com>
2020-11-16 17:46:27 -0500

    Update project.js

--- src/components/project.js
+++ src/components/project.js
@@ -2,20 +2,24 @@ import React, { Component } from 'react';
 import Fade from 'react-reveal/Fade';
 
 class Project extends Component {
-    state = {  }
-    render() { 
-        return ( 
-        <Fade bottom>
-        <div className='project'>
-        <a target="_blank" href={this.props.url}>
-            <img src={this.props.imageSrc} alt={this.props.title}></img>
-            </a>
-            <h1>{this.props.title}</h1>
-            <p><span>{this.props.description}</span></p>
-            <span>{this.props.service}</span>
-        </div> 
+    state = {}
+    render() {
+        return (
+            <Fade bottom>
+                <div className='project'>
+                    <a target="blank" href={this.props.url}>
+                        <img src={this.props.imageSrc} alt={this.props.title}></img>
+                    </a>
+                    <h1>{this.props.title}</h1>
+                    <p><span>{this.props.description}</span></p>
+                    <p>{this.props.service}</p>
+                    
+                    <button class='projectBtn'><a target="_blank" href={this.props.urlCode} alt={this.props.urlCode}> Github</a></button>
+
+                </div>
             </Fade>);
     }
 }
- 
+
 export default Project;
+
