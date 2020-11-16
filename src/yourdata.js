667b9e8acadf29c355ee7367c7b85d337e92e91f
Kristy Keller <keller.kristy@outlook.com>
2020-11-16 17:47:18 -0500

    Update yourdata.js

--- src/yourdata.js
+++ src/yourdata.js
@@ -1,4 +1,4 @@
-
+/* eslint-disable import/no-anonymous-default-export */
 export default
     {
         headerTagline: [//Line 1 For Header
@@ -25,21 +25,21 @@ export default
         projects: [
             {
                 id: 1,
-                title: 'Engineer Team Profile Generator', 
-                description: "A software engineering team generator command line application. Prompts the user for information about the team manager and then information about the team members. The user can input any number of team members, and they may be a mix of engineers and interns. When the user has completed building the team, the application creates an HTML file that displays a formatted team roster based on the information provided by the user.",
-                service: "HTML5, CSS3, JavaScript, Jest.js testing, Node.js, Express.js, Github API, CLI application",
-                imageSrc: "https://i.ibb.co/q5dR8fy/team-gen-screenshot.png",
-                url: 'https://github.com/KristyKeller/TeamGenerator',
-                code: ""
-            },
-            {
-                id: 2,
                 title: 'Ice Cream Truck ORM',
                 description: "A ice cream logger app with MySQL, Node, Express, Handlebars and a homemade ORM that lets users input the names of ice cream flavors they'd like to eat. Whenever a user submits a ice cream flavor, the app will display the ice cream flavor on the page. Each ice cream flavor in the waiting area also has a Devour button. When the user clicks Devour, the ice cream will move to the right side of the page. This app stores every ice cream in a database, whether devoured or not.",
                 service: "HTML5, Handlebars.js, CSS3, JavaScript, Homemade ORM, Node.js, Express, MySQL",
                 imageSrc: "https://i.ibb.co/3h1KPZv/Screenshot-2.png",
-                url: 'https://github.com/KristyKeller/IceCreamTruck',
-                code: ""
+                url: 'https://ice-cream-truck.herokuapp.com/',
+                urlCode: "https://github.com/KristyKeller/IceCreamTruck"
+            },
+            {
+                id: 2,
+                title: 'Engineer Team Profile Generator', 
+                description: "A software engineering team generator command line application. Prompts the user for information about the team manager and then information about the team members. The user can input any number of team members, and they may be a mix of engineers and interns. When the user has completed building the team, the application creates an HTML file that displays a formatted team roster based on the information provided by the user.",
+                service: "HTML5, CSS3, JavaScript, Jest.js testing, Node.js, Express.js, Github API, CLI application",
+                imageSrc: "https://i.ibb.co/q5dR8fy/team-gen-screenshot.png",
+                url: 'https://drive.google.com/file/d/1bhLu0rQtZ7xfDRMs5FQ1AdVNLG6L_58Y/view',
+                urlCode: "https://github.com/KristyKeller/TeamGenerator"
             },
             {
                 id: 3,
@@ -47,8 +47,8 @@ export default
                 description:"An application that lets the user search for any show or movie they'd like to stream. This application shows where this show or movie is currently streaming, the release date, and user ratings. The user can save the shows and movies they want to watch to a watch list viewable once they sign into their account. The user can mark which content they've finished watching and delete from their watch list at any time.",
                 service: "HTML5, CSS3, JavaScript, JQuery, Handlebars.js, MySQL, JavaScript, API, Node.js",
                 imageSrc: "https://i.ibb.co/DrHyHkn/screencapture-stream-saver-herokuapp-watchlist-2020-10-18-23-44-00.png",
-                url: 'https://github.com/KristyKeller/Stream-Saver',
-                code: ""
+                url: 'https://stream-saver.herokuapp.com/',
+                urlCode: "https://github.com/KristyKeller/Stream-Saver"
             },
             {
                 id: 4,
@@ -56,8 +56,8 @@ export default
                 description:"Work day planner developed to create a schedule based on standard business hours where the current date and time is displayed. Time blocks are color coded to indicate whether the event is in the past, present, or future. The user's entered schedule is saved in local storage and stored when the page is refreshed.",
                 service: "HTML5, CSS3, Javascript, Moment.js",
                 imageSrc: "https://i.ibb.co/WvV1BwN/day-planner.png",
-                url: 'https://github.com/KristyKeller/DayPlanner',
-                code:""
+                url: 'https://kristykeller.github.io/DayPlanner/',
+                urlCode: "https://github.com/KristyKeller/DayPlanner"
             },
             {
                 id: 5,
@@ -66,16 +66,16 @@ export default
                 service:"JSX, CSS3, React, Express, MongoDB, Node,js, Bcrypt, Passport, JSON",
                 imageSrc: "https://i.ibb.co/bLSddVz/screenshot5-Copy.png",
                 url: 'http://codecoach0.herokuapp.com/',
+                urlCode: "https://github.com/KristyKeller/CodeCoach"
             },
-
             {
                 id: 6,
                 title: 'Weather Dashboard',
                 description:"A weather dashboard that allows the user to search for a city that is then displayed to his or her search history. Current and future weather forecasts are presented through a 5-day forecast. Each day has the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index. The UV index is assigned a color that showcases whether the conditions are favorable, moderate, or severe. The user can navigate using previously searched cities via the search history list.",
                 service: "HTML5, CSS3, Javascript, API, Moment.js",
                 imageSrc: 'https://i.ibb.co/XpMgQGc/weather-screenshot.png',
-                url: 'https://github.com/KristyKeller/WeatherDashboard',
-                code: ""
+                url: 'https://kristykeller.github.io/WeatherDashboard/',
+                urlCode: "https://github.com/KristyKeller/WeatherDashboard"
             }
         ],
         social: [
@@ -87,6 +87,7 @@ export default
             {
                 name: 'Github',
                 url: 'https://github.com/KristyKeller'
+            
             },
             {
                 name: 'LinkedIn',
